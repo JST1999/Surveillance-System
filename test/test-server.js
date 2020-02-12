6573ac245efdf569c94c30e8c8ab3c9e72b9b0f2
JST1999 <47395303+JST1999@users.noreply.github.com>
2020-02-12 02:08:09 +0000

    tried some bug fixing and chai testing. will continue with it later

--- test/test-server.js
+++ test/test-server.js
@@ -4,22 +4,52 @@ var server = require('../express-server.js');
 var createHash = require('../express-server.js').createHash;
 var createSalt = require('../express-server.js').createSalt;
 
-var sessID = "";
-
 chai.use(chaiHttp);
 
-// describe('[THING]', function() {
-//   it('should list ALL [INSERT THING HERE] on /listitems GET', function(done) {
-//     this.timeout(20000);
-//     setTimeout(done, 20000);//my parents internet and my laptop does this test at around 5000-9000ms
-//     chai.request(server)
-//       .get('/[THING]')
-//       .end(function(err, res){
-//         res.should.have.status(200);
-//         res.should.be.json;
-//         res.body.should.be.a('array');
-//         res.body[0].should.have.property('_id');
-//         done();
-//       });
-//   });
-// });
+describe('Hashing', function() {
+    it('should generate hashes correctly', function(done) {
+      this.timeout(20000);
+      setTimeout(done, 20000);
+      var password = "password123";
+      var salt = createSalt();
+      var hash = createHash(password, salt);
+      var password2 = "password123";
+      var salt2 = salt
+      var hash2 = createHash(password2, salt2);
+  
+      hash.should.equal(hash2);
+      done();
+    });
+});
+
+describe('User', function() {
+    var adminSessID = 0;
+    var itemID = 0;
+    it('should confirm login details of SINGLE User and return a session ID on /adminlogin POST', function(done) {
+      this.timeout(20000);
+      setTimeout(done, 20000);
+      chai.request(server)
+        .post('/adminlogin')
+        .send({username: "jstungay",
+              password: "password"})
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.message.should.have.length(64);
+          adminSessID = res.body.message;
+          done();
+        });
+    });
+    it('should confirm a logout of SINGLE User on /logout POST', function(done) {
+      this.timeout(20000);
+      setTimeout(done, 20000);
+      chai.request(server)
+        .post('/logout')
+        .send({sessionID: adminSessID})
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          done();
+        });
+    });
+});
