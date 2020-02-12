96dd2fd6b50c48fc82260ea5e764d2632a5a863a
Jason Tungay <jtungay@gmail.com>
2020-02-12 17:12:49 +0000

    Did some chai testing, and some heroku ci

--- test/test-server.js
+++ test/test-server.js
@@ -6,6 +6,10 @@ var createSalt = require('../express-server.js').createSalt;
 
 chai.use(chaiHttp);
 
+// const { assert } = require('chai');
+// const { expect } = require('chai');
+var should = chai.should();//vs code says it isnt used, but it is
+
 describe('Hashing', function() {
     it('should generate hashes correctly', function(done) {
       this.timeout(20000);
@@ -16,15 +20,15 @@ describe('Hashing', function() {
       var password2 = "password123";
       var salt2 = salt
       var hash2 = createHash(password2, salt2);
-  
-      hash.should.equal(hash2);
+      
+      hash.should.equal(hash2);//assert.equal(hash, hash2);
       done();
     });
 });
 
 describe('User', function() {
     var adminSessID = 0;
-    var itemID = 0;
+
     it('should confirm login details of SINGLE User and return a session ID on /adminlogin POST', function(done) {
       this.timeout(20000);
       setTimeout(done, 20000);
@@ -37,6 +41,23 @@ describe('User', function() {
           res.should.be.json;
           res.body.message.should.have.length(64);
           adminSessID = res.body.message;
+          done();
+        });
+    });
+    it('should confirm login details of SINGLE User and return a session ID on /adminlogin POST', function(done) {
+      this.timeout(20000);
+      setTimeout(done, 20000);
+      chai.request(server)
+        .post('/getadmindetails')
+        .send({sessionID: adminSessID})
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.have.property("firstname");
+          res.body.should.have.property("lastname");
+          res.body.should.have.property("username");
+          res.body.password.should.have.length(0);//a way of saying is empty, theres other ways of doing this
+          res.body.salt.should.have.length(0);
           done();
         });
     });
