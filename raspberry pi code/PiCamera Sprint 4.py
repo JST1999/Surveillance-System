77b337ed0534e0c3dc0dd9973ed95b1566e8462a
JST1999 <47395303+JST1999@users.noreply.github.com>
2020-03-13 17:41:01 +0000

    rasp pi: user sessionid can be saved, the user can logout aswell. I made it more efficient by using functions for login, logout and the main part of the program. also set the default resolution to 1080p instead of auto, also increased the shutter speed to reduce motion blur

--- raspberry pi code/PiCamera Sprint 4.py
+++ raspberry pi code/PiCamera Sprint 4.py
@@ -12,58 +12,105 @@ gpio.setup(13,gpio.OUT)
 camera = picamera.PiCamera()
 camera.vflip = True#it is flipped by default so this flips it back
 camera.hflip = True
+camera.exposure_mode = "sports"#faster shutter speed so less motion blur
+camera.resolution = (1920, 1080)#1080p
 
-file = open("number.txt", 'r+')
-i = int(file.read())#used to give each pic its own unique name
-file.close()
+sessionFile = open("sessionID.txt", 'r+')
+sessID = sessionFile.read()#used to give each pic its own unique name
+sessionFile.close()
 
 
-#this section is the login section
-url = "https://surv-system.herokuapp.com/adminlogin"
-sessionID = ""
-while True:
-    username = input("Username:")
-    password = input("Password:")
+#this section is the login section, it will retrieve a session ID
+def login():
+    url = "https://surv-system.herokuapp.com/adminlogin"
+    while True:
+        username = input("Username:")
+        password = input("Password:")
+        
+        data = {
+            "username":username,
+            "password":password
+        }
+        
+        try:
+            r = requests.post(url, data=data)
+        except:
+            print("Network error: server or your connection may be down")
+        status = r.status_code
+        if status == 200:
+            json = r.json()
+            sessionID = json["message"]
+            
+            sessionFile = open("sessionID.txt", 'w')#the w method will overwrite the previous value, whereas r+ doesn't
+            sessionFile.write(sessionID)#non-volatile storage of session ID
+            sessionFile.close()#saves changes
+            
+            return sessionID
+        elif status == 401:
+            print("Invalid login credentials")
+        else:
+            print("Something went wrong, try again")
+
+
+#logs out a user
+def logout(sessionID):
+    open("sessionID.txt", 'w').close()#the open('w') opens and deletes everything in the file. close() saves it and closes it
+    file = open("number.txt", 'w')#these 3 set the txt to 0
+    file.write(str(0))
+    file.close()
     
+    url = "https://surv-system.herokuapp.com/logout"
     data = {
-        "username":username,
-        "password":password
+        "sessionID":sessionID
     }
-    
     try:
         r = requests.post(url, data=data)
+        print("Logged-out. Login below:")
     except:
         print("Network error: server or your connection may be down")
-    status = r.status_code
-    if status == 200:
-        json = r.json()
-        sessionID = json["message"]
-        print("Got session. Now running")
-        break
-    elif status == 401:
-        print("Invalid login credentials")
-    else:
-        print("Something went wrong, try again")
 
 
 #this section is the one that checks the IR sensor and takes a picture, also post reqs it
-url = "https://surv-system.herokuapp.com/addimage"
-while True:
-    if gpio.input(11) == True:#can be one, if ir sensor sends a signal it will be 1/true
-        gpio.output(13, True)#led on
-        
-        name = sessionID + "%s.jpg" % i#e.g. image0.jpg, image1.jpg
-        
-        camera.capture(name)#saved into the same directory. If an image.jpg already exists, then it will be overwritten
-        
-        files = {"imgUploader": open(name, 'rb')}
-        requests.post(url, files=files)#post req
-        
-        i += 1
-        file = open("number.txt", 'w')#the w method will overwrite the previous value, whereas r+ doesn't
-        file.write(str(i))#non-volatile storage of what image number I'm on
-        file.close()#saves changes
-    else:
-        gpio.output(13, False)#led off
+def main(sessionID):
+    print("Now running...")
     
-gpio.cleanup()
+    file = open("number.txt", 'r+')
+    i = int(file.read())#used to give each pic its own unique name
+    file.close()
+    
+    url = "https://surv-system.herokuapp.com/addimage"
+    while True:
+        if gpio.input(11) == True:#can be one, if ir sensor sends a signal it will be 1/true
+            gpio.output(13, True)#led on
+            
+            name = sessionID + "%s.jpg" % i#e.g. image0.jpg, image1.jpg
+            
+            camera.capture(name)#saved into the same directory. If an image.jpg already exists, then it will be overwritten
+            
+            files = {"imgUploader": open(name, 'rb')}
+            requests.post(url, files=files)#post req
+            
+            i += 1
+            file = open("number.txt", 'w')#the w method will overwrite the previous value, whereas r+ doesn't
+            file.write(str(i))#non-volatile storage of what image number I'm on
+            file.close()#saves changes
+        else:
+            gpio.output(13, False)#led off
+        
+    gpio.cleanup()
+
+
+while True:
+    if not sessID:#if sessID is empty
+        print("No sessionID was found. Login below:")
+        main(login())
+    else:
+        answer = input("SessionID was found. Continue with current session ID(Y or N):")
+        answer = answer.upper()
+        if answer == 'Y':
+            main(sessID)
+        elif answer == 'N':
+            logout(sessID)
+            main(login())
+        else:
+            print("Invalid input")
