5da085c7623250f13c999db9bfc85dcf6cbde959
JST1999 <47395303+JST1999@users.noreply.github.com>
2020-03-20 21:49:00 +0000

    updated the modules this time

--- express-server.js
+++ express-server.js
@@ -115,53 +115,8 @@ var videoStorage = multer.diskStorage({
 			if (sess){// session found
 				schemas.Admin.findOne({"_id": sess.userID}, function(err, user) {//get user
 					callback(null, name);
-
-					try{
-						ffprobe("./videos/"+name, { path: ffprobeStatic.path }, function (err, metadata) {
-							var Video = new schemas.Video({
-								"username": user.username,
-								"year": year,
-								"month": month,
-								"day": day,
-								"hour": hour,
-								"minute": min,
-								"second": sec,
-								"millisecond": millisecond,
-								"filename": "./videos/"+name,
-								"video_streams": [
-									{
-										"duration": "[duration]",
-										"bitrate": "[bitrate]",
-										"fps":	"[frames_per_second]",
-										"resolution":	"[resolution]"
-									}
-								]
-							});
-							
-							var video = [];
-							for (i = 0; i < metadata["streams"]["length"]; i++){
-								if (metadata["streams"][i]["codec_type"] === "video"){//there should be only one stream, which would be a video one but ive just got this to check to make sure
-									var str = metadata["streams"][i]["avg_frame_rate"];
-									var arr = str.split('/');
-									var fps = arr[0] / arr[1];
-									video = video.concat({
-										duration: metadata["streams"][i]["duration"],
-										bitrate: metadata["streams"][i]["bit_rate"],
-										fps: fps,
-										resolution: metadata["streams"][i]["width"]+"x"+metadata["streams"][i]["height"]
-									});
-								}
-							}
-							Video.video_streams = video;
-		
-							Video.save();
-		
-							//console.dir(Video);
-							//console.dir(metadata);
-						});
-					}
-					catch(err){
-						console.log(err.message);
+					
+					ffprobe("./videos/"+name, { path: ffprobeStatic.path }, function (err, metadata) {
 						var Video = new schemas.Video({
 							"username": user.username,
 							"year": year,
@@ -181,8 +136,28 @@ var videoStorage = multer.diskStorage({
 								}
 							]
 						});
+						
+						var video = [];
+						for (i = 0; i < metadata["streams"]["length"]; i++){
+							if (metadata["streams"][i]["codec_type"] === "video"){//there should be only one stream, which would be a video one but ive just got this to check to make sure
+								var str = metadata["streams"][i]["avg_frame_rate"];
+								var arr = str.split('/');
+								var fps = arr[0] / arr[1];
+								video = video.concat({
+									duration: metadata["streams"][i]["duration"],
+									bitrate: metadata["streams"][i]["bit_rate"],
+									fps: fps,
+									resolution: metadata["streams"][i]["width"]+"x"+metadata["streams"][i]["height"]
+								});
+							}
+						}
+						Video.video_streams = video;
+	
 						Video.save();
-					}
+	
+						//console.dir(Video);
+						//console.dir(metadata);
+					});
 
 				});
 			}
