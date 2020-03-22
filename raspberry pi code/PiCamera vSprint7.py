64e0be96a12065cae64c85204e6ee266613a0190
JST1999 <47395303+JST1999@users.noreply.github.com>
2020-03-22 00:05:59 +0000

    did this a long time ago, strangley had problems gettin the files from the sd card. Can now upload videos to the server, everything works e.c.t. had to convert the files from h264 to mp4 on the pi. Also the user can pick which mode to use e.c.t.

--- raspberry pi code/PiCamera vSprint7.py
+++ raspberry pi code/PiCamera vSprint7.py
@@ -2,6 +2,7 @@ import picamera
 import RPi.GPIO as gpio
 import requests
 import json
+from os import system
 
 gpio.setwarnings(False)#now the pi ignores them
 
@@ -56,6 +57,9 @@ def login():
 def logout(sessionID):
     open("sessionID.txt", 'w').close()#the open('w') opens and deletes everything in the file. close() saves it and closes it
     file = open("number.txt", 'w')#these 3 set the txt to 0
+    file.write(str(0))
+    file.close()
+    file = open("numberVideo.txt", 'w')#these 3 set the txt to 0
     file.write(str(0))
     file.close()
     
@@ -126,23 +130,131 @@ def main_offline():
     gpio.cleanup()
 
 
+def main_video(sessionID):
+    print("Now running...")
+    
+    file = open("numberVideo.txt", 'r+')
+    i = int(file.read())#used to give each pic its own unique name
+    file.close()
+    
+    camera.resolution = (480, 360)
+    
+    url = "https://surv-system.herokuapp.com/addvideo"
+    while True:
+        if gpio.input(11) == True:#can be one, if ir sensor sends a signal it will be 1/true
+            gpio.output(13, True)#led on
+            
+            name = sessionID + "%s.h264" % i#e.g. video0.h264
+            mp4Name = sessionID + "%s.mp4" % i
+            
+            camera.start_recording(name)
+            
+            while gpio.input(11):#if true, the loop will run
+                pass#do nothing, pass is used to help satisfy syntax
+            
+            camera.stop_recording()
+            
+            gpio.output(13, False)#led off
+            
+            command = "MP4Box -add " + name + " " + mp4Name#converts h264 to mp4
+            system(command)#unix cmd
+            
+            files = {"vidUploader": open(mp4Name, 'rb')}
+            requests.post(url, files=files)#post req
+            
+            i += 1
+            file = open("numberVideo.txt", 'w')#the w method will overwrite the previous value, whereas r+ doesn't
+            file.write(str(i))#non-volatile storage of what image number I'm on
+            file.close()#saves changes
+        
+    gpio.cleanup()
+
+
+def main_video_offline():
+    print("Now running...")
+    
+    file = open("numberVideoOffline.txt", 'r+')
+    i = int(file.read())#used to give each pic its own unique name
+    file.close()
+    
+    camera.resolution = (480, 360)
+    
+    while True:
+        if gpio.input(11) == True:#can be one, if ir sensor sends a signal it will be 1/true
+            gpio.output(13, True)#led on
+            
+            name = "video" + "%s.h264" % i#e.g. video0.h264
+            
+            camera.start_recording(name)
+            
+            while gpio.input(11):#if true, the loop will run
+                pass#do nothing, pass is used to help satisfy syntax
+            
+            camera.stop_recording()
+            
+            gpio.output(13, False)#led off
+            
+            i += 1
+            file = open("numberVideoOffline.txt", 'w')#the w method will overwrite the previous value, whereas r+ doesn't
+            file.write(str(i))#non-volatile storage of what image number I'm on
+            file.close()#saves changes
+        
+    gpio.cleanup()
+
+
+#video or image mode decider
+def image_mode():
+    while True:
+        imgOrVid = input("0 for image | 1 for video:")
+        if imgOrVid == "0":
+            return True
+        elif imgOrVid == "1":
+            return False
+        else:
+            print("Invalid input")
+
+
 while True:
     onOrOff = input("0 for offline | 1 for online:")
     if onOrOff == "0":#as a string, there will be no need for try except in case the value cant be converted to an int e.g. int("a")
-        main_offline()
-    elif onOrOff == "1":
-        if not sessID:#if sessID is empty
-            print("No sessionID was found. Login below:")
-            main(login())
+        imageMode = image_mode()
+        if imageMode == True:
+            print("Image mode")
+            main_offline()
         else:
-            answer = input("SessionID was found. Continue with current session ID(Y or N):")
-            answer = answer.upper()
-            if answer == 'Y':
-                main(sessID)
-            elif answer == 'N':
-                logout(sessID)
+            print("Video mode")
+            main_video_offline()
+    elif onOrOff == "1":
+        imageMode = image_mode()
+        if imageMode == True:
+            print("Image mode")
+            if not sessID:#if sessID is empty
+                print("No sessionID was found. Login below:")
                 main(login())
             else:
-                print("Invalid input")
+                answer = input("SessionID was found. Continue with current session ID(Y or N):")
+                answer = answer.upper()
+                if answer == 'Y':
+                    main(sessID)
+                elif answer == 'N':
+                    logout(sessID)
+                    main(login())
+                else:
+                    print("Invalid input")
+        else:
+            print("Video mode")
+            if not sessID:#if sessID is empty
+                print("No sessionID was found. Login below:")
+                main_video(login())
+            else:
+                answer = input("SessionID was found. Continue with current session ID(Y or N):")
+                answer = answer.upper()
+                if answer == 'Y':
+                    main_video(sessID)
+                elif answer == 'N':
+                    logout(sessID)
+                    main_video(login())
+                else:
+                    print("Invalid input")
     else:
         print("Invalid input")
